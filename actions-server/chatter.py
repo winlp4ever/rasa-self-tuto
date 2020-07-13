15efd987e8a04a7b5735f8d0ab0275afaecaa0d5
ha quang le <winlp4ever@gmail.com>
2020-07-13 23:48:03 +0200

    add stress test

--- actions-server/chatter.py
+++ actions-server/chatter.py
@@ -1,8 +1,22 @@
-import requests
+import grequests
+import time
+import random
 
-res = requests.post('http://localhost:5005/webhooks/rest/webhook', json={
-    "message":"c'est quoi html", 
-    "sender": "quang"
-})
+exec_stack = []
+nb_requests = 50
 
-print(res.json())
+count = [0]
+
+def log():
+    count[0] += 1
+    print(count[0])
+
+for i in range(nb_requests):
+    exec_stack.append(grequests.post('http://localhost:5005/webhooks/rest/webhook', json={
+        "message":"c'est quoi html", 
+        "sender": i
+    }, hooks=dict(response=log)))
+
+st = time.time()
+grequests.map(exec_stack)
+print('number of requests: %d - time-lapse: %.2f' % (nb_requests, time.time() - st))
